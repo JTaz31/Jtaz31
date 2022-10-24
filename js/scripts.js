2e24625ef1194272bfbb5a9142b6491659f739c2
elizastuglik <elizastuglik6798@gmail.com>
2022-10-24 18:11:14 +0200

    s

--- js/scripts.js
+++ js/scripts.js
@@ -6,6 +6,23 @@
 //
 // Scripts
 // 
+document.addEventListener('DOMContentLoaded', () => {
+    "use strict";
+  
+    /**
+     * Preloader
+     */
+    const preloader = document.querySelector('#preloader');
+    if (preloader) {
+      window.addEventListener('load', () => {
+        setTimeout(() => {
+          preloader.classList.add('loaded');
+        }, 1000);
+        setTimeout(() => {
+          preloader.remove();
+        }, 2000);
+      });
+    }
 
 window.addEventListener('DOMContentLoaded', event => {
 
